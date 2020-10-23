757200507c15887c44d3b55a4fc50513ed9f20a4
ZeroTixDev <weddnremy@gmail.com>
2020-10-23 22:08:27 +0000

    added server upgrade listener

--- server/index.js
+++ server/index.js
@@ -5,6 +5,11 @@ const path = require("path");
 const msgpack = require("msgpack-lite");
 const app = express();
 const wss = new WebSocket.Server({ noServer: true });
+const server = app.listen(4000);
 app.use(express.static("client"));
 app.get("/", (_, res) => res.sendFile("client/index.html"));
-console.log("running")
+server.on('upgrade', (request, socket, head) => {
+  wss.handleUpgrade(request, socket, head, socket => {
+    wss.emit('connection', socket, request);
+  });
+});
